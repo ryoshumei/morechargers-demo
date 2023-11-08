996269f25d77c1177228919a201614355247b849
daydreamer <s419922572@gmail.com>
2023-11-08 20:18:50 +0900

    20231108

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Logo and links -->
+    </nav>
+</template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleMap } from 'vue3-google-map';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(GoogleMap, {
+    load: {
+        key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    },
+});
+
+app.mount('#app')
