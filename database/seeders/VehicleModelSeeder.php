d46ffdc42dff5ba2ac7cfb0bb99e21eaf253f979
daydreamer <s419922572@gmail.com>
2023-11-25 12:46:09 +0900

    renamed model to vehicle_model

--- database/seeders/VehicleModelSeeder.php
+++ database/seeders/VehicleModelSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ModelSeeder extends Seeder
+class VehicleModelSeeder extends Seeder
 {
     /**
      * Run the database seeds.
