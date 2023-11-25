7f1afa80359ddfbf804a35ffb549ec93b470ffa4
daydreamer <s419922572@gmail.com>
2023-11-25 12:39:17 +0900

    updated database migrations

---
+++ app/Models/ChargerType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChargerType extends Model
+{
+    use HasFactory;
+}
