6e0dae804511f13d2e642e96157207831a5e7881
daydreamer <s419922572@gmail.com>
2023-11-26 20:55:17 +0900

    init controllers

---
+++ app/Http/Controllers/ProviderCompanyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProviderCompany;
+use Illuminate\Http\Request;
+
+class ProviderCompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProviderCompany $providerCompany)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProviderCompany $providerCompany)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProviderCompany $providerCompany)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProviderCompany $providerCompany)
+    {
+        //
+    }
+}
